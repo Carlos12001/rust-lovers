569a38375016f0fb4ac159605a62b53bac926cbe
Carlos12001 <carlos.andres12001@gmail.com>
2025-03-22 23:59:48 -0600

    Remove target directory from version control

--- practice/p0_stack/target/debug/deps/p0_stack.d
+++
@@ -1,5 +0,0 @@
-C:\Users\carlos\Repos\rust-lovers\practice\p0_stack\target\debug\deps\p0_stack.exe: src\main.rs
-
-C:\Users\carlos\Repos\rust-lovers\practice\p0_stack\target\debug\deps\p0_stack.d: src\main.rs
-
-src\main.rs:
--- practice/p0_stack/target/debug/p0_stack.d
+++
@@ -1,1 +0,0 @@
-C:\Users\carlos\Repos\rust-lovers\practice\p0_stack\target\debug\p0_stack.exe: C:\Users\carlos\Repos\rust-lovers\practice\p0_stack\src\main.rs
